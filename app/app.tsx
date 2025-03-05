74eed7dd05f2854bd1eb0a5004efa3be5420f223
jeancarlosZ <64503277+jeancarlosZ@users.noreply.github.com>
2025-03-04 20:51:54 -0500

    refactor particles effect code

--- app/app.tsx
+++ app/app.tsx
@@ -2,95 +2,12 @@ import Navigation from 'app/components/Navigation/Navigation';
 import Logo from 'app/components/Logo/Logo';
 import ImageLinkForm from 'app/components/ImageLinkForm/ImageLinkForm';
 import Rank from 'app/components/Rank/Rank';
-import { useEffect, useMemo, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import {
-  type Container,
-  type ISourceOptions,
-  MoveDirection,
-  OutMode,
-} from "@tsparticles/engine";
-import { loadSlim } from "@tsparticles/slim";
+import ParticlesEffect from 'app/components/ParticlesEffect/ParticlesEffect';
 
 export function App() {
-  const [init, setInit] = useState(false);
-
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
-
-  const particlesLoaded = async (container?: Container): Promise<void> => {
-    console.log(container);
-  };
-
-  const options: ISourceOptions = useMemo(
-    () => ({
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 5,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.5,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: '#FFD700',
-        },
-        move: {
-          direction: MoveDirection.outside,
-          enable: true,
-          outModes: {
-            default: OutMode.out,
-          },
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 150,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "star",
-        },
-        size: {
-          value: { min: 5, max: 10 },
-        },
-      },
-      detectRetina: true,
-    }),
-    [],
-  );
-
   return (
     <main>
-      {init && <Particles
-        className='particles'
-        id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={options}
-      />}
+      <ParticlesEffect />
       <Navigation />
       <Logo />
       <Rank />
---
+++ app/components/ParticlesEffect/ParticlesEffect.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesEffect = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 5,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.5,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#FFD700",
+        },
+        move: {
+          direction: "outside",
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "star",
+        },
+        size: {
+          value: { min: 5, max: 10 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        className="particles"
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+}
+
+export default ParticlesEffect;
